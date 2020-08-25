e67c7ec1d7bbeec9c5928f0daa4c6257210f0b0b
buzaidj <66746824+buzaidj@users.noreply.github.com>
2020-08-25 17:00:03 -0400

    Add files via upload

--- email-reader.py
+++ email-reader.py
@@ -1,74 +1,261 @@
 from imapclient import IMAPClient
+import imapclient.exceptions
 import json, time
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta, timezone
 import queue
 import email
 from email import policy
 
-import printing
-import schedule
+import pytz
+from tzlocal import get_localzone
 
-username = input('Username: ')
-password = input('Password: ')
+import phonenumbers
+
+
+from escpos.printer import Usb, Dummy
+
+# TODO: CHANGE TO INPUT FXN
+username = input('username')
+password = input('password')
+
+
+# TODO : Add support for printing at a certain time using the orders_to_print queue/printing 10 mins before we want it to
+
 
 class Order:
+    needed_params = ['Name', 'Number', 'Order', 'Notes']
+
+    email_time_format = '%a, %d %b %Y %H:%M:%S %z'
+    date_format = "%b %-d"
+    time_format = "%-I:%M:%S %p"
+
+    tz = None
+    while tz == None:
+        try:
+            tz = get_localzone()
+
+        except Exception as e:
+            print(e)
+            print("Trying to get time zone again in 15 seconds")
+            time.sleep(15)
+
+
     """
-    Class to represent an order and print them
+    Class to represent an order with a printer tied to it
+
+    :param msgid: msg identifier number
+    :param raw_email: raw email in bytes
     """
     def __init__(self, msgid, raw_email):
         """
         Initializes an order from a raw email and msg id
         """
         email_message = email.message_from_bytes(raw_email[b'RFC822'], policy = policy.default)
+        self.order_type = email_message.get('Subject')
         body = str(email_message.get_body('text/plain'))
         body = body[body.find('{'): body.find('}') + 1]
 
-        self.order_number = msgid
-        self.can_print = True
+        self.order_number = msgid % 100
+        self.can_print = 'full'
 
         try:
             self.order_dict = json.loads(body)
-        except:
+            try:
+                for x in self.needed_params:
+                    assert x in self.order_dict
+            except Exception as e:
+                print(e)
+                self.can_print = 'limited'
+                # SECONDARY PRINTING OF MISFORMATTED MESSAGE THAT CAN STILL BE READ
+
+        except Exception as e:
             self.order_dict = {}
-            self.can_print = False
+            self.can_print = 'message format error'
 
-        self.date = email_message.get('Date')
+        dt_str = str(email_message.get('Date'))
+        self.dt = datetime.strptime(dt_str, self.email_time_format).astimezone(self.tz)
+        self.date = self.dt.strftime(self.date_format)
+        self.time = self.dt.strftime(self.time_format)
 
-    def print_order(self):
-        printing.print_line("Order #", self.order_number)
-        if self.can_print:
-            for k, v in self.order_dict.items():
-                printing.print_line(k, v)
+
+    """
+    Print order to thermal receipt printer with instantiated order class
+
+    :param p: instantiated connected printer
+    """
+    def print_order(self, p):
+        if self.can_print == 'full':
+            print_ticket(p, self.order_dict, self.order_type, self.order_number, self.date, self.time)
+        elif self.can_print == 'limited':
+            print_ticket_limited(p, self.order_dict, self.order_type, self.order_number, self.date, self.time)
         else:
-            printing.print_text_line("Order not formatted correctly")
-
-        printing.print_blank()
+            print_error_message(p, 'message formatted incorrectly')
 
 
 
-def run_service(today, orderTimes, username, password):
-    can_order = orderTimes.order_range[0] <= datetime.now().time() and (datetime.now() + timedelta(minutes = 2)).time() <= orderTimes.order_range[1]
+"""
+Print an error message to thermal receipt printer
 
-    already_ordered = {}
+:param p: instantiated connected printer
+:param msg: message (as a string)
+"""
+def print_error_message(p, msg):
+    try:
+        d = Dummy()
+
+        d.set(align = "left", bold = True, underline = 2, double_height = False, double_width = False, invert = True, density = 4, font = 'a')
+        d.textln('ERROR: ' + msg)
+        d.cut()
+
+        p._raw(d.output)
+
+    except Exception as e:
+        print(e)
+
+"""
+Print a ticket to thermal receipt printer
+
+:param p: instantiated connected printer
+:param order_dict: dictionary of the order
+    MUST have keys for "name, type, order, notes"
+"""
+def print_ticket(p, order_dict, order_type, order_number, order_date, order_time):
+    try:
+        d = Dummy()
+        d.line_spacing(120)
+
+        name = order_dict.pop('Name')
+        number = order_dict.pop('Number')
+
+        formatted_number = phonenumbers.format_number(phonenumbers.parse(number, 'US'), phonenumbers.PhoneNumberFormat.NATIONAL)
+
+        order = order_dict.pop('Order')
+        notes = order_dict.pop('Notes')
+
+        d.set(align = "center", bold = True, underline = 2, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        d.block_text(str(order_number) + ' ' + name + '\n', font = 'a')
+        d.ln(1)
+
+        d.set(align = "center", bold = False, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        d.block_text(order_type + '\n', columns = 20, font = 'a')
+        d.ln(1)
+
+        d.text(formatted_number + '\n')
+        d.text(order_date + '\n')
+        d.text(order_time + '\n')
+        d.text(' \n')
 
 
-    with IMAPClient(host="imap.gmail.com") as client:
-        client.login(username, password)
-        client.select_folder('INBOX')
+        d.set(align = "left", bold = True, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'b')
+        d.text('ORDER\n')
 
-        # get new messages, print them, then delete them
-        while can_order:
-            print("")
-            print("Reading Orders")
-            new_messages = client.search([['FROM', 'cornellpikesorders@gmail.com']])
-            for msgid, raw_email in client.fetch(new_messages, 'RFC822').items():
-                new_order = Order(msgid, raw_email)
-                new_order.print_order()
+        d.set(align = "left", bold = False, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        d.block_text(order + '\n', columns = 20, font = 'a')
 
-            client.delete_messages(new_messages)
-            client.expunge()
+        d.ln(2)
 
-            time.sleep(30)
-            client.noop()
+        if notes != '':
+            d.set(align = "left", bold = True, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'b')
+            d.text('NOTES\n')
 
-run_service(date.today(), schedule.Schedule["test_full"], username, password)
+            d.set(align = "left", bold = False, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+            d.block_text(notes + '\n', columns = 20, font = 'a')
+
+        d.cut()
+
+        p._raw(d.output)
+
+    except Exception as e:
+        print(e)
+
+def print_ticket_limited(p, order_dict, order_type, order_number, order_date, order_time):
+    try:
+        d = Dummy()
+        d.line_spacing(120)
+
+        d.set(align = "center", bold = True, underline = 2, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        d.block_text(str(order_number) + ' ' + '\n', font = 'a')
+        d.ln(1)
+
+        d.set(align = "center", bold = False, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        d.block_text(order_type + '\n', columns = 20, font = 'a')
+        d.ln(1)
+
+        d.text(order_date + '\n')
+        d.text(order_time + '\n')
+        d.text(' \n')
+
+
+        d.set(align = "left", bold = True, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'b')
+        d.text('ORDER DETAILS\n')
+
+        d.set(align = "left", bold = False, underline = 0, double_height = False, double_width = False, custom_size = True, smooth = True, width = 2, height = 2, invert = False, density = 8, font = 'a')
+        for k, v in order_dict.items():
+            d.block_text(k.capitalize() + ':\t' + v + '\n', columns = 20, font = 'a')
+            d.ln(2)
+
+        d.cut()
+
+        p._raw(d.output)
+
+
+    except Exception as e:
+        print(e)
+
+
+
+
+def run_service(username, password, vendor_id = 0x04b8, product_id = 0x0e02, dummy = False):
+    # initialize printer here
+    if not dummy:
+        p = None
+        while p == None:
+            try:
+                p = Usb(vendor_id, product_id)
+
+            except Exception as e:
+                print(e)
+                print("Trying again in 15 seconds")
+                time.sleep(15)
+
+    else:
+        p = Dummy()
+
+
+
+    can_order = True
+
+    try:
+        with IMAPClient(host="imap.gmail.com") as client:
+            client.login(username, password)
+            client.select_folder('INBOX')
+            while can_order:
+                new_messages = client.search([['FROM', 'cornellpikeskitchen@gmail.com']])
+                for msgid, raw_email in client.fetch(new_messages, 'RFC822').items():
+                    new_order = Order(msgid, raw_email)
+                    new_order.print_order(p)
+
+                client.delete_messages(new_messages)
+                client.expunge()
+
+                time.sleep(15)
+                client.noop()
+
+    except KeyboardInterrupt:
+        # print_error_message(p, "Quitting. Restart Raspberry PI (black switch) to continue reading orders.")
+        raise KeyboardInterrupt;
+
+    except imapclient.exceptions.LoginError as e:
+        print_error_message(p, "Could not connect to internet.")
+
+    except Exception as e:
+        print(e)
+        #just try again after waiting
+        time.sleep(15)
+        run_service(username, password)
+
+
+run_service(username, password)
+
+# 87 - James Buzaid
+#
